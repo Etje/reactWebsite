4f8b327b0538517d6b20656927c69fb1f7b9b6ea
Etje <etien_ruhl@live.nl>
2019-07-03 18:40:06 +0200

    added bootstrap to create form

--- client/src/Create.js
+++ client/src/Create.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import {Container, Col, Form, FormGroup, Label, Input, Button} from 'reactstrap';
 
-class View extends Component {
+
+class Create extends Component {
 
   state = {
       product: {
@@ -24,12 +26,26 @@ class View extends Component {
       const { product } = this.state;
         return (
             <div>
-                <input value={product.productName} onChange={e => this.setState({ product: { ...product, productName: e.target.value} })}/>
-                <input value={product.productPrice}  onChange={e => this.setState({ product: { ...product, productPrice: e.target.value} })}/>
-                <button className="btn btn-primary" onClick={this.addProduct}>Add Product</button>
+                <Form className="form">
+                  <Col>
+                    <FormGroup>
+                      <Label>Product Naam</Label>
+                      <Input value={product.productName} onChange={e => this.setState({ product: { ...product, productName: e.target.value} })}/>
+                    </FormGroup>
+                  </Col>
+                  <Col>
+                    <FormGroup>
+                      <Label>Product Price</Label>
+                      <Input value={product.productPrice}  onChange={e => this.setState({ product: { ...product, productPrice: e.target.value} })}/>
+                    </FormGroup>
+                  </Col>
+                  <Col>
+                    <Button onClick={this.addProduct}>Add Product</Button>
+                  </Col>
+                </Form>
             </div>
         );
     }
 }
 
-export default View;
+export default Create;
